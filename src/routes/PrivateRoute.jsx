d144f39eae0773fd990fb3751c59adc7944eb61d
Sadia492 <sadiaafrin2792>
2024-12-21 21:05:15 +0600

    banner and loading spinner added

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 export default function PrivateRoute({ children }) {
   const { user, loading } = useAuth();
   const location = useLocation();
   if (loading) {
-    return "loading...";
+    return <LoadingSpinner></LoadingSpinner>;
   }
   if (user) {
     return children;
