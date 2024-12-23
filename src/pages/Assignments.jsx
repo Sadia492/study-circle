dbc6c0b64b2fddb60acd4e4b80011b22fb2804e1
Sadia492 <sadiaafrin2792>
2024-12-23 15:58:09 +0600

    assignments page filter and search functionality added

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -1,24 +1,30 @@
 import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import AssignmentCard from "../components/AssignmentCard";
 import Swal from "sweetalert2";
 import useAuth from "../hooks/useAuth";
 
 export default function Assignments() {
   const queryClient = useQueryClient();
+  const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("");
+  // const [searchText, setSearchText] = useState("");
   const { user } = useAuth();
 
   const { data: assignments } = useQuery({
-    queryKey: ["assignments"],
+    queryKey: ["assignments", { filter, search }],
     queryFn: async () => {
       const { data } = await axios.get(
-        `${import.meta.env.VITE_URL}/assignments`
+        `${import.meta.env.VITE_URL}/assignments`,
+        {
+          params: { filter, search },
+        }
       );
       return data;
     },
   });
-  console.log(assignments);
+
   const handleDelete = async (id, email) => {
     if (user.email === email) {
       const result = await Swal.fire({
@@ -70,6 +76,36 @@ export default function Assignments() {
 
   return (
     <div>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-5 ">
+        <div>
+          <select
+            onChange={(e) => {
+              setFilter(e.target.value);
+            }}
+            value={filter}
+            name="difficulty"
+            id="difficulty"
+            className="border p-4 rounded-lg"
+          >
+            <option value="">Filter By Difficulty Level</option>
+            <option value="easy">easy</option>
+            <option value="medium">medium</option>
+            <option value="hard">hard</option>
+          </select>
+        </div>
+
+        <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
+            type="text"
+            name="search"
+            placeholder="Enter Assignment Title"
+            aria-label="Enter Assignment Title"
+          />
+        </div>
+      </div>
       Assignments
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {assignments?.map((assignment) => (
