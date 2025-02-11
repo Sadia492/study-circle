408d8f17d358542ae3d84a939f3fa3cafb578829
Sadia492 <sadiaafrin2792>
2025-02-11 07:01:56 +0600

    newsletter section modified

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -13,7 +13,7 @@ export default function Newsletter() {
       </p>
       <div className="bg-gradient-to-r from-primary to-secondary mb-12 text-white py-16">
         <div className="container mx-auto text-center">
-          <h2 className="text-3xl font-semibold mb-4">
+          <h2 className="text-3xl font-bold mb-4">
             Stay Updated with Our Latest News
           </h2>
           <p className="text-lg mb-8">
